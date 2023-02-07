b83160266bb106c04dd17464a637395aed2d7326
Sudarshana-V <124652592+Sudarshana-V@users.noreply.github.com>
2023-02-07 10:16:24 +0530

    Add files via upload

---
+++ Add_2_num.java
@@ -0,0 +1,11 @@
+import java.io.PrintStream;
+public class Add_2_num
+{
+public static void main(String arg[])
+{
+int a=5;
+int b=70;
+int c=a+b;
+System.out.println("The sum of 5 and 70 is " +c);
+}
+}
---
+++ addition.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class addition
+{
+public static void main(String args[])
+{
+System.out.println("Enter 1st num " +args[0]);
+System.out.println("Enter 2nd num " +args[1]);
+int num1=args[0];
+char num2=args[1];
+char add=num1+num2;
+System.out.println("The sum is " +add);
+}
+}
