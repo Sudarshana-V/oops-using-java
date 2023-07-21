c119cad525fea31eebfc8b8956d91d5ddc92accd
Sudarshana-V <124652592+Sudarshana-V@users.noreply.github.com>
2023-07-21 14:23:39 +0530

    Add files via upload

---
+++ String_to_boolean.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class String_to_boolean
+{
+	public static void main(String args[])
+	{
+		String str="True";
+		boolean bool=Boolean.parseBoolean(str);
+        System.out.print(bool);
+	}
+}	
